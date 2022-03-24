49340050e8fafc0d6d47c41ae703e614b2791d43
AlexGitHubPage <alexandre.torres@mataro.epiaedu.cat>
2022-03-24 15:25:47 +0100

    Juegocompleto

--- Juegodelavida/src/codeprim.java
+++ Juegodelavida/src/codeprim.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class codeprim {
 	static Scanner e = new Scanner(System.in);
 	
-	public char [][] creatablero(){
+	public char [][] creatablero(){ //Metodo que crea la vida o tablero
 		char [][] t = new char [10][10];
 		
 			for( int i = 0; i < t.length; i++) {
@@ -15,7 +15,17 @@ public class codeprim {
 			
 		return t;
 	}
-	public void vertablero( char[][] t){
+	public void delay(long ms) { //PAUSEN 
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void vertablero( char[][] t){ //Visualización del tablero 
 		for( int a = 0; a < t.length; a++) {
 			System.out.println(" ");
 			for( int h = 0; h < t[0].length; h++) {
@@ -25,7 +35,7 @@ public class codeprim {
 		}
 	}
 	
-	public char[][]  CelManual(char [][] t) {
+	public char[][]  CelManual(char [][] t) { //Introducir la celula manualmente 
 		System.out.println("En que fila vas a introducir esa celula? ");
 		int fil = e.nextInt();
 		System.out.println("En que columna vas a introducir la celula?");
@@ -35,7 +45,7 @@ public class codeprim {
 		
 		return t;
 	}
-	public int[][]vecinas(int colu, int fila, char[][]t){ //1, 1
+	public int[][]vecinas(int colu, int fila, char[][]t){ //1, 1 //Detección de las celulas vecinas
 		
 		int[][] crdVecinas = new int[8][2];
 
@@ -91,7 +101,7 @@ public class codeprim {
 		int count = 0;
 		
 		for(int j = 0; j<crdVecinas.length; j++) {
-				//Condicion que controla 
+				//Condicion que controla que la vecina no sea negativa o más alta que la longitud del tablero con vida 
 			if(crdVecinas[j][0]>=0 && crdVecinas[j][1]>=0) {
 				
 				if(crdVecinas[j][0]<t[0].length && crdVecinas[j][1]<t.length) {
@@ -108,9 +118,10 @@ public class codeprim {
 		}
 		
 		
-		return Arrays.copyOfRange(crdVecinasClone, 0, count);
+		return Arrays.copyOfRange(crdVecinasClone, 0, count); //Del rango desde 0 hasta el contador de las vecinas contadas
 	}
-	public int vivas( char[][]t , int[][]crdVecinas){
+	
+	public int vivas( char[][]t , int[][]crdVecinas){ //Total de vecinas para controlar el status de una celula concreta 
 		int vivas = 0;	
 		
 			for( int k = 0; k < crdVecinas.length; k++) {
@@ -126,22 +137,36 @@ public class codeprim {
 		
 		return vivas;
 	}
-	public char [][] play(char[][] t){
+	
+	public char[][] play(char[][] t){
+		char [][] ident = new char [t.length][t[0].length];
+		
+		for( int i = 0; i<ident.length; i++) for( int j = 0; j<ident[0].length; j++) ident[i][j] = t[i][j];
 		
 		for( int i = 0; i<t.length; i++) {
+			
 			for(int x = 0; x<t[0].length; x++) {
-				int[][] crdVecinas = vecinas(i, x, t);
-				int numVivas = vivas(t, crdVecinas);
-				System.out.println(" ");
-				System.out.println("Celda: X: " + x + " Y: " + i + " Vecinas vivas: " + numVivas);
+				
+				int[][] crdVecinas = vecinas(i, x, ident);
+				
+				int numVivas = vivas(ident, crdVecinas);
+				
+				if( ident[i][x] == 'X' && !( numVivas == 3 || numVivas == 2) ){
+						
+					t[i][x] = '-';
+				}
+				else if( ident[i][x] == '-' && numVivas == 3 ) {
+					t[i][x] = 'X';
+				}
+				
 			}
 		}
 		
-		
 		return t;
+	
 	}
 	
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args)  {
 		codeprim p = new codeprim();
 		// TODO Auto-generated method stub"
 		
@@ -187,28 +212,29 @@ public class codeprim {
 				System.out.println("2:CREAR CÉLULAS MANUALMENTE: ");
 				System.out.println("3:CREAR CÉLULAS AUTOMATICAMENTE: ");
 				System.out.println("4:SETTINGS: ");
-				System.out.println("5:CREAR VIDA: ");
-				System.out.println("6:VISUALIZAR VIDA: ");
-				System.out.println("7:EXiT? ");
+				System.out.println("5:CREAR VIDA (CUIDADO!! ESTO DESHARÁ LOS CAMBIOS DE VIDA PREVIA) : ");
+				System.out.println("6:JUGAR: ");
+				System.out.println("7: VISUALIZAR SOLO EL MAPA: ");
+				System.out.println("8:EXiT? ");
 			int option = e.nextInt();	
 			switch(option) {	
 				case 1: 
 					System.out.println("HOLA! ESTAS SON LAS REGLAS DEL JUEGO DE LA VIDA: ");
-					Thread.sleep(2000);
+					p.delay(2000);
 					System.out.println("-- EN EL JUEGO EXISTEN CÉLULAS VIVAS O MUERTAS EN EL TABLERO ");
-					Thread.sleep(2000);
+					p.delay(2000);
 					System.out.println("-- CADA CÉLULA TIENE 8 POSIBLES CÉLULAS VECINAS QUE LA RODEAN ");
-					Thread.sleep(2000);
+					p.delay(2000);
 					System.out.println("-- EN CADA GENERACIÓN O LAS CÉLULAS CAMBIAN, ES DECIR VIVEN O...MUEREN :) ");
-					Thread.sleep(2000);
+					p.delay(2000);
 					System.out.println("-- SI HAY UNA CÉLULA QUE TIENE 2 O 3 CÉLULAS VECINAS ESTA VIVE\\");
-					Thread.sleep(2000);
+					p.delay(2000);
 					System.out.println("-- SI HAY UNA CÉLULA QUE TIENE 4 O MÁS CÉLULAS VECINAS ESTA MUERE\\");
-					Thread.sleep(2000);
+					p.delay(2000);
 					System.out.println("-- SI HAY UNA CÉLULA QUE TIENE 1 O 0 CÉLULAS VECINAS ESTA TAMBIÉN MUERE (POR SOLEDAD) \\");
-					Thread.sleep(2000);
+					p.delay(2000);
 					System.out.println("-- SI HAY UNA CÉLULA MUERTA  QUE TIENE 3 CÉLULAS VECINAS VIVAS ESTA REVIVE\\");
-					Thread.sleep(3000);
+					p.delay(3000);
 					System.out.println("Y...ESTAS SON TODAS LAS REGLAS DIVIERTETE!");
 					enmenu = true;
 					break;
@@ -228,17 +254,35 @@ public class codeprim {
 					
 				case 5:
 					//Crear
+					System.out.println("CUIDADO!! ESTO DESHARÁ LOS CAMBIOS DE VIDA");
 					p.creatablero();
 						enmenu = true;
 						break;
 				case 6:
-					//Visualizar
+					//Play
+					boolean ingame = true;
+					int gen = 0;
+					
 					p.vertablero(life);
-					p.play(life);
+					
+					while(ingame) { 
+						
+					p.vertablero(p.play(life));
+					p.delay(100);
+					gen++;
+					System.out.println(" ");
+					System.out.println("Generación: " + gen);
+					
+					}
 					break;
-				case 7:	
+				case 7: 
+					//Visual
+					p.vertablero(life);
+					
+					break;
+				case 8:	
 					System.out.println("SALIENDO...");
-					Thread.sleep(4000);
+					p.delay(4000);
 					enmenu = false;
 					break;
 					
